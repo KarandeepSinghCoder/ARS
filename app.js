a2943efdd5e8d83586679fa5bbd1881f9689a41e
Sanskrita Saha <sanskrita2001@gmail.com>
2020-12-12 17:40:19 +0530

    Login Page

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //Requirements
-var express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  passport = require("passport"),
-  localStrategy = require("passport-local"),
-  passportLocalMongoose = require("passport-local-mongoose");
-const { check, validationResult } = require("express-validator");
-const colors = require("colors");
-const dotenv = require("dotenv");
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	localStrategy = require('passport-local'),
+	passportLocalMongoose = require('passport-local-mongoose');
+const { check, validationResult } = require('express-validator');
+const colors = require('colors');
+const dotenv = require('dotenv');
 
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
 
 //Modles to be included
 
 //Modles ends
-app.use("/", express.static("public"));
+app.use('/', express.static('public'));
 
 //PORT ADDRESS
 var port = process.env.PORT || 3001;
@@ -26,28 +26,30 @@ connectDB();
 
 //app.use
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //RESTFUL ROUTES
-app.get("/", function(req, res) {
-  res.render("index");
+app.get('/', function (req, res) {
+	res.render('index');
 });
-app.get("/findorgan", function(req, res) {
-  //form to be submited so as to search database
-  res.render("findorgan");
+app.get('/findorgan', function (req, res) {
+	//form to be submited so as to search database
+	res.render('findorgan');
 });
-app.get("/organ", function(req, res) {
-  //to display all the organs
-  res.render("organ");
-});
-
-app.get("/hospitallogin", function(req, res) {
-  res.render("hospitallogin");
-});
-app.get("/hospitaldashboard", function(req, res) {
-  res.render("hospitaldashboard");
+app.get('/organ', function (req, res) {
+	//to display all the organs
+	res.render('organ');
 });
 
+app.get('/hospitallogin', function (req, res) {
+	res.render('hospitallogin');
+});
+app.get('/hospitaldashboard', function (req, res) {
+	res.render('hospitaldashboard');
+});
+app.get('/register', function (req, res) {
+	res.render('register.ejs');
+});
 //Hospital login
 // app.get("/login", function(req, res) {
 //   res.render("login.ejs");
@@ -58,6 +60,6 @@ app.get("/hospitaldashboard", function(req, res) {
 // });
 
 //Server
-app.listen(port, function() {
-  console.log("server is on".yellow.bold);
+app.listen(port, function () {
+	console.log('server is on'.yellow.bold);
 });
